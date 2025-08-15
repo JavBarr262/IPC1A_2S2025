cd6951582e9f05a03eed02e790fd45522eef2b4b
JavBarr262 <3014026980101@ingenieria.usac.edu.gt>
2025-08-15 02:54:53 -0600

    tercer commit

--- Practica1.java
+++ Practica1.java
@@ -8,18 +8,20 @@ package com.mycompany.practica1;
  *
  * @author USUARIO
  */
-
+//utilizo la libreria de scanner para poder leer los datos de la lista de manera sencilla 
 import java.util.Scanner;
 public class Practica1{
 private static final Scanner scanner = new Scanner(System.in);
 
+  private static int Creacionpersonajes=0;
+  private static int Cantidadpersonajes=0;
    //Creamos los vecotores que vayan a contener la informacion necesitada
-  private static String[] nombrep = new String[100];
-  private static String[] arma = new String[100];
+  private static String[] nombres=new String[100];
+  private static String[] arma=new String[100];
   //Nos pidieron como maximo que se pudierna asignar 5 habilidades como maximo
-  private static String[][] habilidades = new String[100][5];
-  private static int[] nivelpersonaje = new int[100];
-  private static int Creacionpersonajes= 0;
+  private static String[][] habilidades=new String[100][5];
+  private static int[] nivelpersonaje=new int[100];
+
  
   public static void main(String[] args){
   Menu();
@@ -38,7 +40,7 @@ private static final Scanner scanner = new Scanner(System.in);
   System.out.println("Ver historial de las peleas realizadas=7");
   System.out.println("Terminar proceso=8");
 try{
-    validacion = Integer.parseInt(scanner.nextLine());
+    validacion=Integer.parseInt(scanner.nextLine());
     switch(validacion){
         case 1:
             Creacionpersonajes();
@@ -70,36 +72,59 @@ try{
 }catch (NumberFormatException error) {
         System.out.println("Seleccione un numero de la lista porfavor");
       }
-  } while(validacion !=8);
+  } while(validacion!=8);
   }
  
 
     public static void Creacionpersonajes(){
-              System.out.println("Creacion de personajes");
+        if (Cantidadpersonajes>=100) {
+            System.out.println("Limite de personajes alcanzado ");
+            return;
+        }
 
+        System.out.println("\n***Creacion personajes***");
+        String nombrep;
+        //usamos un boolean para poder verificar que los nombres no se repitan
+       boolean validacionnombres=false;
+       do{
+           System.out.print("Ingrese nombre del pesonjae:");
+           nombrep = scanner.nextLine();
+           if(nombrep.isEmpty()){
+               System.out.println("Porfavor ingrese algun nombre para le personaje que va a ser creado");
+               continue;
+           }
+           validacionnombres=true;
+           for(int a=0; a<Cantidadpersonajes;a++){
+               if(nombres[a].equalsIgnoreCase(nombrep)){
+               System.out.println("El nombre ya existe ecriba uno diferente porfavor");
+              validacionnombres=false;
+              break;
+               }
+           }
+          }while (!validacionnombres);
 }
     public static void Verinfopersonajes(){
-              System.out.println("ver info personajes");
+              System.out.println("*ver info personajes*");
 
 }
     public static void Verlistapersonajes(){
-              System.out.println("ver lista personajes");
+              System.out.println("*ver lista personajes*");
 
 }
     public static void Pelea(){
-              System.out.println("Pelea");
+              System.out.println("*Pelea*");
 
 }
        public static void Editarinfo(){
-              System.out.println("Editar informacion");
+              System.out.println("*Editar informacion*");
 
 }
     public static void Eliminar(){
-              System.out.println("Eliminar");
+              System.out.println("*Eliminar*");
 
 }
     public static void Verhistorial(){
-              System.out.println("Eliminar");
+              System.out.println("*Ver historial*");
 
 }
   
